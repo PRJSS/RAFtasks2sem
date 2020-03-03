f64e9a4f6e0af516104ea13d48452d789b4189a6
Danila Bernat <danila6545@icloud.com>
2020-03-04 00:11:35 +0300

    x3 chto

--- лаба 5 артем/задача 6.1/задача 6.1/Function.h
+++ лаба 5 артем/задача 6.1/задача 6.1/Function.h
@@ -13,5 +13,6 @@ double Simpson(double (*equation)(double, double, double),
 	double argument = 1,
 	double argument1 = 0.5,
 	double esp = 0.000001,
-	int Number_of_partitions = 2);//уравнение для которого проводим рассчёты
+	int Number_of_partitions = 2);
 double equation(double x, double s, double t);
+
